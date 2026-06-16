42cf7c1188c7c0b710c4d8f56a62cfd694dea95a
malhotrankit <89144724+malhotrankit@users.noreply.github.com>
2026-06-16 13:46:04 +0200

    Add: count-connected-components - submission-3

---
+++ Data Structures & Algorithms/count-connected-components/submission-3.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        int i,count=0;
+        vector<vector<int>> g(n);
+        vector<bool> visited(n,false);
+        for(auto it:edges){
+            g[it[0]].push_back(it[1]);
+            g[it[1]].push_back(it[0]);
+        }
+        for(i=0;i<n;i++){
+            if(!visited[i]){
+                dfs(i,g,visited,-1);
+                count++;
+            }
+        }
+        return count;
+    }
+    void dfs(int node, vector<vector<int>>& g,vector<bool>& visited,int parent){
+        if(visited[node]==true)
+            return;
+        visited[node]=true;
+        for(auto it:g[node]){
+            //if(it==parent)
+                //continue;
+            dfs(it,g,visited,node);
+        }
+    }
+};
